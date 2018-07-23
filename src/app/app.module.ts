4fc0a225bbf95a263481bc8916b495f3a0e9c5df
ershad <ershad@gmail.com>
2018-07-23 12:54:04 +0600

    Polishing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from './share/shared/shared.module';
+import {HttpClientModule} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import {SharedModule} from './share/shared/shared.module';
     HomeComponent,
   ],
   imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule
   ],
--- src/app/share/shared/shared.module.ts
+++ src/app/share/shared/shared.module.ts
@@ -1,24 +1,15 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MaterialModule} from '../material/material.module';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     MaterialModule,
     FlexLayoutModule,
-    CommonModule,
-    BrowserModule,
-    BrowserAnimationsModule,
   ],
   exports: [
-    CommonModule,
     MaterialModule,
     FlexLayoutModule,
-    BrowserModule,
-    BrowserAnimationsModule,
   ]
 })
 export class SharedModule {
